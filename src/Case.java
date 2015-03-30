ba4aad251025f06f49a8d8338d215b2f3e18f4f9
loewb <benoit.loew@iut-valence.fr>
2015-03-30 17:29:08 +0200

    nouvelles classes

---
+++ src/Case.java
@@ -0,0 +1,5 @@
+
+public class Case {
+Coordonnees coordonnes;
+TypeCase typeCase;
+}
---
+++ src/Coordonnees.java
@@ -0,0 +1,12 @@
+
+public class Coordonnees {
+private static int axeX;
+private static int axeY;
+
+public int getAxeX() {
+	return axeX;
+}
+public int getAxeY() {
+	return axeY;
+}
+}
