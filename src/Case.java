aa711ad7b24fa551e621448f140303db6de1439b
loewb <benoit.loew@iut-valence.fr>
2015-04-10 10:23:42 +0200

    update case

--- src/Case.java
+++ src/Case.java
@@ -2,7 +2,40 @@
 /* TODO JAVADOC. */
 public class Case {
     /* TODO JAVADOC. */
-    Coordonnees coordonnes;
+    private Coordonnees coordonnes;
     /* TODO JAVADOC. */
-    TypeCase typeCase;
+    private TypeCase typeCase;
+    
+    private boolean BoatBool;
+    
+    private boolean CheckerBool;
+    
+    public Case (Coordonnees coor,TypeCase type)
+    {
+    	this.coordonnes = coor;
+    	this.typeCase = type;
+    	this.BoatBool = false;
+    	this.CheckerBool = false;
+    }
+    
+	public Coordonnees getCoordonnes() 
+	{
+		return coordonnes;
+	}
+	
+	public TypeCase getTypeCase() 
+	{
+		return typeCase;
+	}
+	
+	public boolean isBoatBool() 
+	{
+		return BoatBool;
+	}
+	
+	public boolean isCheckerBool() 
+	{
+		return CheckerBool;
+	}
+    
 }
