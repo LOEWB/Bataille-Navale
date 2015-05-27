4be56d9d602c0875f760ea42989b71b67edd8afb
LOEWB <benoit.loew@iut-valence.fr>
2015-05-28 00:44:47 +0200

    few changes

--- src/fr/iutvalence/loewb/model/battleship/Starter.java
+++ src/fr/iutvalence/loewb/model/battleship/Starter.java
@@ -20,6 +20,6 @@ public class Starter
   public static void main(String args[])
   {
     // Game game = new Game();
-    Window window = new Window();
+	Window window = new Window();
   }
 }
--- src/fr/iutvalence/loewb/view/gui/Window.java
+++ src/fr/iutvalence/loewb/view/gui/Window.java
@@ -16,63 +16,65 @@ import javax.swing.JTextField;
 public class Window extends JFrame
 {
 
-  private JSplitPane splitQuitter, splitGo, splitJoueurs;
+	private JPanel container;
+	private JSplitPane splitQuit, splitGo, splitJoueurs;
+	private JTextField tfJoueur1 = new JTextField("Joueur1");
+	private JTextField tfJoueur2 = new JTextField("Joueur2");
+	private JButton bQuitter = new JButton("Quitter");
+	private JButton bGo = new JButton("GO");
 
-  private JTextField tfJoueur1 = new JTextField("Joueur1");
-  private JTextField tfJoueur2 = new JTextField("Joueur2");
-  private JButton bQuitter = new JButton("Quitter");
-  private JButton bGo = new JButton("GO");
+	public Window()
+	{
 
-  public Window()
-  {
+		this.setSize(1100, 700);
+		this.setLocationRelativeTo(null);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    this.setSize(1000, 700);
-    this.setLocationRelativeTo(null);
+		JPanel quitter = new JPanel();
+		quitter.setPreferredSize(new Dimension(1000, 400));
+		JPanel go = new JPanel();
+		JPanel joueur1 = new JPanel();
+		JPanel joueur2 = new JPanel();
+		quitter.setBackground(Color.red);
+		go.setBackground(Color.blue);
+		joueur1.setBackground(Color.white);
+		joueur2.setBackground(Color.white);
 
-    JPanel quitter = new JPanel();
-    quitter.setPreferredSize(new Dimension(1000, 400));
-    JPanel go = new JPanel();
-    JPanel joueur1 = new JPanel();
-    JPanel joueur2 = new JPanel();
-    quitter.setBackground(Color.red);
-    go.setBackground(Color.blue);
-    joueur1.setBackground(Color.white);
-    joueur2.setBackground(Color.white);
+		splitJoueurs = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, joueur1, joueur2);
+		splitJoueurs.setDividerSize(1);
+		splitJoueurs.setDividerLocation(this.getWidth()/2);
 
-    splitJoueurs = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, joueur1, joueur2);
-    splitJoueurs.setDividerSize(1);
-    splitJoueurs.setDividerLocation(500);
+		splitGo = new JSplitPane(JSplitPane.VERTICAL_SPLIT, splitJoueurs, go);
+		splitGo.setDividerSize(0);
+		splitGo.setDividerLocation(100);
 
-    splitGo = new JSplitPane(JSplitPane.VERTICAL_SPLIT, splitJoueurs, go);
-    splitGo.setDividerSize(0);
-    splitGo.setDividerLocation(100);
+		splitQuit = new JSplitPane(JSplitPane.VERTICAL_SPLIT, quitter, splitGo);
+		splitQuit.setDividerSize(0);
+		splitQuit.setDividerLocation(400);
 
-    splitQuitter = new JSplitPane(JSplitPane.VERTICAL_SPLIT, quitter, splitGo);
-    splitQuitter.setDividerSize(0);
-    splitQuitter.setDividerLocation(400);
+		Font police = new Font("Arial", Font.BOLD, 14);
+		joueur1.setFont(police);
+		joueur1.setPreferredSize(new Dimension(150, 30));
+		joueur1.setForeground(Color.BLUE);
 
-    Font police = new Font("Arial", Font.BOLD, 14);
-    joueur1.setFont(police);
-    joueur1.setPreferredSize(new Dimension(150, 30));
-    joueur1.setForeground(Color.BLUE);
+		joueur2.setFont(police);
+		joueur2.setPreferredSize(new Dimension(150, 30));
+		joueur2.setForeground(Color.BLUE);
 
-    joueur2.setFont(police);
-    joueur2.setPreferredSize(new Dimension(150, 30));
-    joueur2.setForeground(Color.BLUE);
+		GridBagConstraints gridStart = new GridBagConstraints();
+		gridStart.gridx = 0;
+		gridStart.gridy = 0;
+		gridStart.gridheight = 1;
+		gridStart.gridwidth = 1;
 
-    GridBagConstraints gridStart = new GridBagConstraints();
-    gridStart.gridx = 0;
-    gridStart.gridy = 0;
-    gridStart.gridheight = 1;
-    gridStart.gridwidth = 1;
+		quitter.add(bQuitter, gridStart);
+		go.add(bGo);
+		joueur1.add(tfJoueur1);
+		joueur2.add(tfJoueur2);
 
-    quitter.add(bQuitter, gridStart);
-    go.add(bGo);
-    joueur1.add(tfJoueur1);
-    joueur2.add(tfJoueur2);
+		this.getContentPane().add(splitQuit);
+		this.setVisible(true);
 
-    this.getContentPane().add(splitQuitter);
-    this.setVisible(true);
-
-  }
+	}
 }
